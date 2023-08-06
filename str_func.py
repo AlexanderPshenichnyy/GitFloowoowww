13b9c8501ac71d04d56ac6826d9b9561dc867b2e
Alexander Pshenichnyy <ter.anti@mail.ru>
2023-08-06 19:39:28 +0300

    Add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def get_upper(text):
+	"""
+fff
+	:param text:
+	:return:
+	"""
 	return text.upper()
+
+
+def get_title(textx):
+	"""
+	возвращает заглавныи первые буквы каждого слова в строке
+	"""
+	return textx.title()
